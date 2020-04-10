6ef6b1d9a1f3ffdd4b3404b8e28edee9b36f16d7
Maurice <mauricelecordier@gmail.com>
2020-04-10 12:39:56 +0200

    moaaaar gatsby additions

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,9 @@
 import React from "react"
-import Header from "../components/header"
+import Layout from '../components/layout';
 
 export default () => (
-  <div style={{ color: `teal` }}>
-    <Header headerText="About Gatsby" />
-    <p>Such wow. Very React.</p>
-  </div>
+  <Layout>
+    <h1>About me</h1>
+    <p>I am out here learning Gatsby</p>
+  </Layout>
 )
